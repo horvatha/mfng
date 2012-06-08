7917d0e03894bd74f532a2be45d3264657c2b618
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2012-06-08 14:01:51 +0200

    scripts/mfngnohup.sh bugfix: output does not need a runs directory

--- scripts/mfngnohup.sh
+++ scripts/mfngnohup.sh
@@ -4,10 +4,10 @@
 
 if test $# -ge 1
 then FILE=$1
-else FILE=temp77
+else FILE=nohupout
 fi
 
 echo "This script runs the mfng with nohup (no hang up)."
 echo "If we run it on a server and log out the process will no hang up."
 echo "Results will be in ${FILE}_results.txt, details in ${FILE}_details.txt and errors in ${FILE}_err.txt."
-nohup ./mrun.py ${FILE} >> runs/${FILE}_details.txt 2>> runs/${FILE}_err.txt < /dev/null &
+nohup ./mrun.py ${FILE} >> ${FILE}_details.txt 2>> ${FILE}_err.txt < /dev/null &
