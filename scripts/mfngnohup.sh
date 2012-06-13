bdcd15b8d8ec5a9da5844afcf9cc4d557f8c20f0
Horvath Arpad <horvath.arpad.szfvar@gmail.com>
2012-06-13 13:44:59 +0200

    scripts/mfngnohup.sh remove obsolete things

--- scripts/mfngnohup.sh
+++ scripts/mfngnohup.sh
@@ -9,5 +9,5 @@ fi
 
 echo "This script runs the mfng with nohup (no hang up)."
 echo "If we run it on a server and log out the process will no hang up."
-echo "Results will be in ${FILE}_results.txt, details in ${FILE}_details.txt and errors in ${FILE}_err.txt."
-nohup ./mrun.py ${FILE} >> ${FILE}_details.txt 2>> ${FILE}_err.txt < /dev/null &
+echo "Results will be in project*/runs.* files, details in ${FILE}_details.txt and errors in ${FILE}_err.txt."
+nohup ./mrun.py >> runs/${FILE}_details.txt 2>> runs/${FILE}_err.txt < /dev/null &
